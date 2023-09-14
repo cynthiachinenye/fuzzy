b6efe69eb7e5c0e925f2b7b8f8e500be7b740c8c
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-15 00:54:48 +0100

    new

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function Header() {
+const [movie, setMovie] = useState([])
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
